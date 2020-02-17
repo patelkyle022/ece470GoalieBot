a063e9d5a1cfd8119dbfa9b968468d0c72deaeed
jmeyer330 <32105903+jmeyer330@users.noreply.github.com>
2020-02-16 22:46:45 -0600

    Add files via upload
    Simulation Scene and Python Code

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-from move_helper import *
 import sim
 import sys
 import math
@@ -9,7 +8,7 @@ PI = math.pi
 
 
 sim.simxFinish(-1)
-clientID = sim.simxStart('127.0.0.1',19999,True,True,5000,5)
+clientID=sim.simxStart('127.0.0.1',19999,True,True,5000,5)
 if clientID!=-1:
     print ('Connected to remote API server')
 else:
@@ -18,17 +17,41 @@ else:
 
 jointHandles = [-1, -1, -1, -1, -1, -1]
 
-getJointHandles(clientID, jointHandles)
+#Get joint handles
+for i in range(0,6):
+    jointHandles[i] = sim.simxGetObjectHandle(clientID, 'UR3_joint' + str(i + 1), sim.simx_opmode_blocking) [1]
 
 print(jointHandles)
 
+#Get Proximity Sensor Handle
+errorCode, sensorHandle = sim.simxGetObjectHandle(clientID, 'Ball_Sensor', sim.simx_opmode_blocking)
+print(sensorHandle)
 
-targetPos1 = [90*PI/180, 90*PI/180, -90*PI/180, 90*PI/180, 90*PI/180, 90*PI/180]
+
+#Set a target position for movement
+targetPos1 = [45*PI/180, -90*PI/180, 0, 0, 0, 0]
+
+
 
 sim.simxStartSimulation(clientID, sim.simx_opmode_oneshot)
 time.sleep(1)
 
-setTargetPosition(clientID, jointHandles, targetPos1)
+#Read Prox Sensor
+errorCode, detectionState, detectedPoint, detectedObjectHandle, detectedSNV = sim.simxReadProximitySensor(clientID, sensorHandle, sim.simx_opmode_streaming)
+print("Before Movement: " + str(detectionState))
+
+time.sleep(1)
+
+
+#Send command for moving robot.
+for i in range(0,6):
+    sim.simxSetJointTargetPosition(clientID, jointHandles[i], targetPos1[i], sim.simx_opmode_oneshot_wait)
+
+time.sleep(1)
+
+#Read Prox Sensor
+(errorCode, detectionState, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector) = sim.simxReadProximitySensor(clientID, sensorHandle, sim.simx_opmode_streaming)
+print("After Movement: " + str(detectionState))
 
 sim.simxStopSimulation(clientID, sim.simx_opmode_oneshot)
 sim.simxFinish(clientID)
