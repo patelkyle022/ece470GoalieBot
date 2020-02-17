5fe1e131891bf6963ac58f981261764f45b20763
Unnat Ramjiyani <unnatr2@illinois.edu>
2020-02-16 21:15:31 -0600

    Added test.py

---
+++ test.py
@@ -0,0 +1,34 @@
+import sim
+import sys
+import math
+import time
+
+global PI
+PI = math.pi
+
+
+sim.simxFinish(-1)
+clientID=sim.simxStart('127.0.0.1',19999,True,True,5000,5)
+if clientID!=-1:
+    print ('Connected to remote API server')
+else:
+    print ('Failed connecting to remote API server')
+    sys.exit('Could not connect to remote API server')
+
+jointHandles = [-1, -1, -1, -1, -1, -1]
+
+for i in range(0,6):
+    jointHandles[i] = sim.simxGetObjectHandle(clientID, 'UR3_joint' + str(i + 1), sim.simx_opmode_blocking) [1]
+
+print(jointHandles)
+
+
+targetPos1 = [90*PI/180, 90*PI/180, -90*PI/180, 90*PI/180, 90*PI/180, 90*PI/180]
+
+sim.simxStartSimulation(clientID, sim.simx_opmode_oneshot)
+time.sleep(1)
+for i in range(0,6):
+    sim.simxSetJointTargetPosition(clientID, jointHandles[i], targetPos1[i], sim.simx_opmode_streaming)
+
+sim.simxStopSimulation(clientID, sim.simx_opmode_oneshot)
+sim.simxFinish(clientID)
